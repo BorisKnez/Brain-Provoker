b30c5b546dbd3ee2cb64f157e1bc8e9ff7027f76
boriskn <boris.knez2@gmail.com>
2011-04-12 17:55:00 +0200

    nov projekt, dodan na github

---
+++ src/edu/boris/brainprovoker/android/Brain_provoker_main.java
@@ -0,0 +1,23 @@
+package edu.boris.brainprovoker.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+//globalne deklaracije spremenljivk
+
+
+public class Brain_provoker_main extends Activity {
+	
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    
+    
+    
+    
+}
